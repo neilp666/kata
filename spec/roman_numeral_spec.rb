8a5ebc0bdb32bcdb28be8bbc3536d39e7e97e5df
neil patel <neil@myindianweddingapp.com>
2018-06-14 10:59:52 +0100

    Add more tests

--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -6,5 +6,17 @@ RSpec.describe Integer do
     it "converts 1 to 'I'" do
       expect(1.roman_numeral).to eq "I"
     end
+
+    it "converts 5 to 'V'" do
+      expect(5.roman_numeral).to eq "V"
+    end
+
+    it "converts 10 to 'X'" do
+      expect(10.roman_numeral).to eq "X"
+    end
+
+    it "converts 20 to 'XX'" do
+      expect(20.roman_numeral).to eq "XX"
+    end
   end
 end
