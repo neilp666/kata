9af77bad3b1a98b0e4e55077cee4cafb91e82621
neil patel <neil@myindianweddingapp.com>
2018-06-17 15:32:20 +0100

    Adding edge case tests

--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -18,5 +18,21 @@ RSpec.describe Integer do
     it "converts 20 to 'XX'" do
       expect(20.roman_numeral).to eq "XX"
     end
+
+    it "Displays error if you convert '0'" do
+      expect(0.roman_numeral). to eq "Please enter number between 1 and 3999."
+    end
+
+    it "Displays error if you convert a negative number '-1'" do
+      expect(-1.roman_numeral). to eq "Please enter number between 1 and 3999."
+    end
+
+    it "Displays error if convert '4000'" do
+      expect(4000.roman_numeral). to eq "Please enter number between 1 and 3999."
+    end
+
+    it "Displays error if you convert 'A'" do
+      expect(A.roman_numeral). to eq "Please enter number between 1 and 3999."
+   end
   end
 end
